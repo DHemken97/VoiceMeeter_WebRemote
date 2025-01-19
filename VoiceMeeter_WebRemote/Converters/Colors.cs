d38c47619f9c12b569724ce7e7b6088350851a24
= <=>
2025-01-18 20:11:26 -0600

    Add project files.

---
+++ VoiceMeeter_WebRemote/Converters/Colors.cs
@@ -0,0 +1,25 @@
+﻿namespace VoiceMeeter_WebRemote.Converters
+{
+    public static class Colors
+    {
+        public static string Fader_Green => "#70c399";
+        public static string Fader_Red => "#f8634d";
+        public static string Background_Dark => "#2c3d4d";
+        public static string Background_Light => "#36495a";
+        public static string Text_Dark => "#506579";
+        public static string Text_Light => "#506579";
+        public static string Text_Error => "#cc2814";
+        public static string Text_Section_Label => "#718ca2";
+        public static string Toggle_Button_Active_Blue => "#68e6f8";
+        public static string Toggle_Button_Active_Red => "#f66051";
+        public static string Toggle_Button_Active_Green => "#70c399";
+        public static string Toggle_Button_Active_Yellow=> "#e8b15f";
+        public static string Toggle_Button_Inactive => "#6f8ca0";
+        public static string Knob_Active => "#5d9f83";
+        public static string Knob_Inactive => Toggle_Button_Inactive;
+
+        // public static string Fader_Green => "";
+        // public static string Fader_Green => "";
+
+    }
+}
---
+++ VoiceMeeter_WebRemote/Converters/InLineConverter.cs
@@ -0,0 +1,9 @@
+﻿namespace VoiceMeeter_WebRemote.Converters
+{
+    public static class InLineConverter
+    {
+        public static string ToPx(this object value) => $"{value}px";
+        public static string ToPercent(this object value) => $"{value}%";
+        public static string Concat(params string[] args) => string.Join("", args) ;
+    }
+}
